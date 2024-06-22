d0f56c220d1ba4d95738f7038946e7fa9a288c71
Anh Bui <lf2foce@gmail.com>
2024-06-23 01:18:27 +0700

    fix bug comma

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -30,7 +30,8 @@ console.log(posts);
             
             <div>{post.name}</div>
           </div>
-        ))}
+        ))};
+
        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
         <div key={image.id} className="w-48 h-48 flex items-center justify-center bg-gray-200 rounded-lg">
           <Link href={`/img/${image.id}`}>
@@ -48,40 +49,4 @@ console.log(posts);
         
         </div>
       </main>
-  );
-
-
-//   return (
-//     <div className="flex flex-wrap justify-center gap-4 p-4">
-//       {images.map((image) => (
-//         <div key={image.id} className="flex h-48 w-48 flex-col">
-//           <Link href={`/img/${image.id}`}>
-//             <Image
-//               src={image.url}
-//               style={{ objectFit: "contain" }}
-//               width={192}
-//               height={192}
-//               alt={image.name}
-//             />
-//           </Link>
-//           <div>{image.name}</div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default async function HomePage() {
-//   return (
-//     <main className="">
-//       <SignedOut>
-//         <div className="h-full w-full text-center text-2xl">
-//           Please sign in above
-//         </div>
-//       </SignedOut>
-//       <SignedIn>
-//         <Images />
-//       </SignedIn>
-//     </main>
-//   );
-// }
+  )};
